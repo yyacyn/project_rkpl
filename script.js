6c7c3828f09e0aadbc3905709250a01cc8a7dbd9
yashin <yashinalfauzysabara@gmail.com>
2024-05-04 16:09:12 +0700

    build order form
    order form + print function done

--- script.js
+++ script.js
@@ -15,7 +15,13 @@ function dropdown() {
     document.querySelector("#side-menu").classList.toggle('hidden')
 }
 
-// function open() {
-//     document.querySelector(".sidebar").classList.toggle('left-[-300px]')
-    
-// }
+function printDiv(divId) {
+    var printContents = document.getElementById(divId).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+}
