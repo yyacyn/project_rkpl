ff0bdf8cc2feb110db6dd713b556e197b43ba058
yashin <yashinalfauzysabara@gmail.com>
2024-05-05 15:47:19 +0700

    update + money page & input form

--- script.js
+++ script.js
@@ -25,3 +25,51 @@ function printDiv(divId) {
 
     document.body.innerHTML = originalContents;
 }
+
+// Get the box icon element
+var boxIcon = document.querySelector('box-icon[name="image-add"]');
+
+// Get the file input element
+var fileInput = document.getElementById('picture');
+
+// Add event listener to box icon for click event
+boxIcon.addEventListener('click', function () {
+    // Trigger click event on file input
+    fileInput.click();
+});
+
+
+function show(x) {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+document.getElementById('expense-button').addEventListener('click', function() {
+    document.getElementById('expense-content').style.display = 'block';
+    document.getElementById('income-content').style.display = 'none';
+});
+
+document.getElementById('income-button').addEventListener('click', function() {
+    document.getElementById('income-content').style.display = 'block';
+    document.getElementById('expense-content').style.display = 'none';
+});
+
+$(document).ready(function () {
+    $("#income-button").click(function () {
+        $("#income-content").show();
+        $("#expense-content").hide();
+        $("#income-button").addClass("button-active");
+        $("#expense-button").removeClass("button-active");
+    });
+
+    $("#expense-button").click(function () {
+        $("#income-content").hide();
+        $("#expense-content").show();
+        $("#income-button").removeClass("button-active");
+        $("#expense-button").addClass("button-active");
+    });
+});
