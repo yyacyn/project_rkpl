d2304257e50a95e5ab0ce56630edb0974213f57a
yashin <yashinalfauzysabara@gmail.com>
2024-05-13 22:09:44 +0700

    springboot

---
+++ adminweb/src/main/java/com/tokoteratai/adminweb/AdminwebApplication.java
@@ -0,0 +1,13 @@
+package com.tokoteratai.adminweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminwebApplication.class, args);
+	}
+
+}
---
+++ adminweb/src/test/java/com/tokoteratai/adminweb/AdminwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokoteratai.adminweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
